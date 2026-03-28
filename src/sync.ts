c8ff9ebc4f2c29f505004473e76efff47f9d0f34
kh4f <kh4f.dev@gmail.com>
2026-03-28 21:20:29 +0300

    feat(sync): add `force` update option to `syncThemePalette`
    Allow updating all themes at once regardless of palette changes.

--- src/sync.ts
+++ src/sync.ts
@@ -4,17 +4,19 @@ import { now, unmarshalCss, updateTheme, type Palette } from './utils'
 const palettePath = 'src/palette.css'
 let prevPalette: Palette = {}
 
-const syncThemePalette = async () => {
+const syncThemePalette = async (force = false) => {
 	const raw = await Bun.file(palettePath).text()
 	const palette = unmarshalCss(raw)
 
 	for (const theme in palette) {
-		if (JSON.stringify(prevPalette[theme]) !== JSON.stringify(palette[theme])) {
+		if (force || JSON.stringify(prevPalette[theme]) !== JSON.stringify(palette[theme])) {
 			console.log(`[${now()}] updating ${theme}`)
 			void updateTheme(theme, palette[theme])
 		}
 	}
 	prevPalette = palette
 }
+
+void syncThemePalette(true)
 
 watch(palettePath, () => void syncThemePalette())
