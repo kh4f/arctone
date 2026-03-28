8a9a6aedb4e930b472b76dc60968ace4e36e17a7
kh4f <kh4f.dev@gmail.com>
2026-03-28 22:13:28 +0300

    refactor(test): reorganize tests in `main.test.ts`
    Restructure test cases for improved clarity and maintainability.

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,20 +1,6 @@
 import { describe, it, test, expect } from 'bun:test'
 import { applyVar, unmarshalCss } from '@/utils'
 
-describe('applyVar', () => {
-	it('replaces RGB', () => {
-		expect(applyVar('"#aabbcc",//1', '1', 'ddffee')).toBe('"#ddffee",//1')
-	})
-
-	it('keeps old alpha when val is RGB', () => {
-		expect(applyVar('"#aabbcc10",//1', '1', 'ddffee')).toBe('"#ddffee10",//1')
-	})
-
-	it('uses alpha from val when val is RGBA', () => {
-		expect(applyVar('"#aabbcc10",//1', '1', 'ddffee20')).toBe('"#ddffee20",//1')
-	})
-})
-
 test('unmarshalCss', () => {
 	expect(unmarshalCss(`
 		.theme1 { --1: #abcdef; --2: #ghijkl; }
@@ -22,5 +8,17 @@ test('unmarshalCss', () => {
 	`)).toEqual({
 		theme1: { 1: 'abcdef', 2: 'ghijkl' },
 		theme2: { 3: 'mnopqr', 4: 'stuvwx' },
+	})
+})
+
+describe('applyVar', () => {
+	it('replaces RGB', () => {
+		expect(applyVar('"#aabbcc",//1', '1', 'ddffee')).toBe('"#ddffee",//1')
+	})
+	it('keeps old alpha when val is RGB', () => {
+		expect(applyVar('"#aabbcc10",//1', '1', 'ddffee')).toBe('"#ddffee10",//1')
+	})
+	it('uses alpha from val when val is RGBA', () => {
+		expect(applyVar('"#aabbcc10",//1', '1', 'ddffee20')).toBe('"#ddffee20",//1')
 	})
 })
